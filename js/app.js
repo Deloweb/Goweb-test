bd234ba2e8c5e3d21813a65b670c4d081eef7173
Deloweb <lucien.delohen@gmail.com>
2020-07-28 23:45:37 +0200

    Multiples modifications et ajouts:
    Modification de la progress bar circulaire
    Ajout du menu au clique du bouton
    Ajout de la police d'écriture
    Mise en forme en vue desktop

--- js/app.js
+++ js/app.js
@@ -14,13 +14,35 @@ const myInterval = setInterval(() => {
 
 // Donut
 
-const numb = document.querySelector(".numb");
-      let counter = 0;
-      setInterval(()=>{
-        if(counter == 60){
-          clearInterval();
-        }else{
-          counter+=1;
-          numb.textContent = counter + "%";
-        }
-      }, 10);
+document.addEventListener("DOMContentLoaded", () => {
+  progressDonutAnim(60, 15);
+});
+
+function progressDonutAnim(percent, interval) {
+  let circle = document.getElementById("circle");
+  let text = document.getElementById("percent");
+  let angle = 0;
+  percent = percent * 4.7;
+
+  window.timer = window.setInterval(() => {
+    circle.setAttribute("stroke-dasharray", angle + ", 20000");
+
+    text.innerHTML = parseInt((angle / 471) * 100) + " %";
+
+    if (angle >= percent) {
+      window.clearInterval(window.timer);
+    }
+    angle += 7;
+  }, interval);
+}
+
+//  Menu
+let menu = document.getElementById("menu");
+
+const showMenu = () => {
+  menu.style.display = "flex";
+};
+
+const closeMenu = () => {
+  menu.style.display = "none";
+};
